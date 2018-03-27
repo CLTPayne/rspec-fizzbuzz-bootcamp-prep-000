67c0064fc45eec957e2a7d7bbec675fba5d0128a
Charlotte Payne <cltpayne@gmail.com>
2018-03-27 22:08:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 def fizzbuzz(num)
   if num % 3 == 0
      "Fizz"
-  end    
+  end 
+  if num % 5 == 0
+    "Buzz"
+  end
+  if num % 3 && 5 == 0
+    "FizzBuzz"
+  end
+  if num % 3 || 5 != 0
+    nil
+  end
 end
